c1ea8f9a6fac5778677afe78fb0f9c5322fbebd1
pawel <ypawelborkowski@gmail.com>
2025-10-23 14:07:55 +0200

    add toString method in Lotto

--- src/main/java/minigames/game/lotto/model/Lotto.java
+++ src/main/java/minigames/game/lotto/model/Lotto.java
@@ -10,4 +10,9 @@ public class Lotto {
     private final Set<Integer> playerNumbers;
     private final int matchedCount;
 
+
+    @Override
+    public String toString() {
+        return String.format("Draw numbers: %s%n Your numbers: %s%n Matched: %s%n", drawNumbers, playerNumbers, matchedCount);
+    }
 }
